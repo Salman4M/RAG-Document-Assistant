a82c426580fe983214478686b00f09b56cdeac6f
Salman4M <salmanmammadli@hotmail.com>
2026-02-27 18:22:03 +0400

    feat: add JWT authentication (register, login, refresh, logout)

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ if __name__ == "__main__":
 
 from fastapi import FastAPI
 from routes.routes import router
+from routes.auth import router as auth_router
 
-app = FastAPI(title="RAG Document Assistant")
+app = FastAPI(
+    title="RAG Document Assistant",
+    swagger_ui_init_oauth={
+    "usePkceWithAuthorizationCodeGrant": True
+    }
+)
 app.include_router(router)
+app.include_router(auth_router)
--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, String, DateTime, Integer
+from sqlalchemy import Column, String, DateTime, Integer,ForeignKey
 from sqlalchemy.sql import func
 from core.database import Base
-
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__ = "users"
@@ -11,3 +11,16 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True),server_default=func.now())
+    
+    refresh_tokens = relationship("RefreshToken",back_populates="user")
+
+class RefreshToken(Base):
+    __tablename__ = "refresh_tokens"
+
+    id = Column(Integer, primary_key=True, index=True)
+    token = Column(String, unique=True, index=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    expires_at = Column(DateTime(timezone=True),nullable=False)
+    created_at = Column(DateTime(timezone=True),server_default=func.now())
+
+    user = relationship("User",back_populates="refresh_tokens")
